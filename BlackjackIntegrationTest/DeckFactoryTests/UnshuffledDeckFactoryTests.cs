1c0d53efdb7d51e53053fc837bcff8fbe6d900bd
seosamhocleirigh <seosamhocleirigh@gmail.com>
2019-09-14 14:35:44 +0100

    Added more tests, remove gitignored files from repo next

--- BlackjackIntegrationTest/DeckFactoryTests/UnshuffledDeckFactoryTests.cs
+++ BlackjackIntegrationTest/DeckFactoryTests/UnshuffledDeckFactoryTests.cs
@@ -33,5 +33,22 @@ namespace BlackjackIntegrationTest.CardFactoryTests
         {
             _generatedDeck.Where(d => d.CardValues.Count() == 1).Count().ShouldBe(48);
         }
+
+        [Fact]
+        public void CardFactory_CardValuesTest()
+        {
+            _generatedDeck.First(d => d.CardValues.Count() == 2).CardValues.ShouldBe(new int[] { 1, 11 });
+        }
+
+        [Fact]
+        public void CardFactory_DoubleValueCards_DescriptionTest()
+        {
+            var doubleValueCards = _generatedDeck.Where(d => d.CardValues.Count() == 2);
+
+            _generatedDeck.Count(d => d.CardName.Equals("Ace of Diamonds", System.StringComparison.Ordinal)).ShouldBe(1);
+            _generatedDeck.Count(d => d.CardName.Equals("Ace of Hearts", System.StringComparison.Ordinal)).ShouldBe(1);
+            _generatedDeck.Count(d => d.CardName.Equals("Ace of Spades", System.StringComparison.Ordinal)).ShouldBe(1);
+            _generatedDeck.Count(d => d.CardName.Equals("Ace of Clubs", System.StringComparison.Ordinal)).ShouldBe(1);
+        }
     }
 }
